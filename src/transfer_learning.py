98fdbf579dffdcccee2ef29299894df5b5afaf53
practicum polymere - Betr. Constantin Seibold <p22g5@i14s37.anthropomatik.kit.edu>
2022-06-14 10:56:36 +0200

    Add SOS support

--- src/transfer_learning.py
+++ src/transfer_learning.py
@@ -9,8 +9,11 @@ import matplotlib.pyplot as plt
 import time
 import os
 import copy
+import numpy as np
 from torch.autograd import Variable
 from src.recycling_dataset import RecyclingDataset
+import warnings
+warnings.simplefilter("ignore", UserWarning)
 from src.stratified_batch import StratifiedBatchSampler
 
 
@@ -31,7 +34,8 @@ class TransferLearningTrainer:
     TB_DIR = STORE_DIR + "runs/"  # tensorboard save directory
     DATA_DIR = STORE_DIR + "data/"  # input data directory
 
-    def __init__(self, data_transforms=None, batch_size=32, shuffle=True, num_workers=4):
+    def __init__(self, data_transforms=None, batch_size=32, shuffle=True, num_workers=4, sos=''):
+        self.sos = sos
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.num_workers = num_workers
@@ -71,13 +75,13 @@ class TransferLearningTrainer:
         Create image datasets
         :return:
         """
-        self.image_datasets = {x: datasets.ImageFolder(os.path.join(self.DATA_DIR, x),
+        self.image_datasets = {x: datasets.ImageFolder(os.path.join(self.DATA_DIR, self.sos, x),
                                                        self.data_transforms[x])
                                for x in ['train', 'test']}
         self.class_names = self.image_datasets['train'].classes
 
     def _create_recycling_image_datasets(self):
-        self.image_datasets = {x: RecyclingDataset(os.path.join(HOME_DIR, "data", x + ".csv"),
+        self.image_datasets = {x: RecyclingDataset(os.path.join(HOME_DIR, "data", self.sos, x + ".csv"),
                                                    os.path.join(STORE_DIR, "data"),
                                                    self.data_transforms[x])
                                for x in ['train', 'test']}
@@ -88,14 +92,22 @@ class TransferLearningTrainer:
         Create data loaders
         :return:
         """
+        y_train = self.image_datasets['train'].img_labels['count'].tolist()
+        class_sample_count = np.array([len(np.where(y_train == t)[0]) for t in np.unique(y_train)])
+        weight = 1. / class_sample_count
+        samples_weight = np.array([weight[t] for t in y_train])
+        samples_weight = torch.from_numpy(samples_weight)
+        sampler = torch.utils.data.WeightedRandomSampler(samples_weight.type('torch.DoubleTensor'), len(samples_weight))
+        samplers = [sampler, None]
         self.dataloaders = {x: torch.utils.data.DataLoader(self.image_datasets[x],
+                                                           #sampler=samplers[i],
                                                            batch_sampler=StratifiedBatchSampler(self.image_datasets[x].img_labels['count'],
                                                                                                 batch_size=self.batch_size,
                                                                                                 shuffle=self.shuffle),
                                                            #batch_size=self.batch_size,
                                                            #shuffle=self.shuffle,
                                                            num_workers=self.num_workers)
-                            for x in ['train', 'test']}
+                            for i, x in enumerate(['train', 'test'])}
 
     def train_model(self, model, criterion, optimizer, scheduler, num_epochs=25, model_name=None, early_stop=True):
         """
@@ -218,7 +230,7 @@ class TransferLearningTrainer:
         print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
         print(f'Best test acc: {best_acc:4f}')
 
-        # load best model weights
+        # load best model weights        
         model.load_state_dict(best_model_wts)
         path = self.MODELS_DIR + model_name + ".pth"
         torch.save(model.state_dict(), path)
@@ -286,7 +298,7 @@ class TransferLearningTrainer:
                 _, pred = torch.max(output, 1)
 
                 if label != pred:
-                    title = self.image_datasets['test'][i]['img_path'][0]
+                    title = self.image_datasets['test'][i]['img_path'][self.image_datasets['test'][i]['img_path'].find('data')+5:]
                     title += '\n' + f'must be {label}, but predicted {class_names[pred]}'
                     if plot:
                         _ = plt.figure(figsize=(4, 4), dpi=140)
@@ -311,9 +323,10 @@ class TransferLearningTrainer:
         :param model_ft: model used for test
         :return:
         """
-        #model_ft.load_state_dict(torch.load("./models/" + model_name))
-
         n_classes = len(self.class_names)
+        
+        model_ft.to(DEVICE)
+        model_ft.eval()
 
         from sklearn.metrics import confusion_matrix
         import seaborn as sn
