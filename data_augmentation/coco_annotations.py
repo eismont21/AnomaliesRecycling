0bba54593f5e233827745c5e210d9e9827b696d0
Charlotte Goos <ch.goos@yahoo.de>
2022-07-03 15:34:43 +0200

    name correction and tried to have right type in json file

--- data_augmentation/coco_annotations.py
+++ data_augmentation/coco_annotations.py
@@ -10,20 +10,20 @@ INFO = {
     'description': 'Synthesized Coco Dataset',
     'url': '',
     'version': '1.0',
-    'year': int(2022),
+    'year': 2022,
     'contributor': 'CVHCIPraktikum',
     'date_created': datetime.datetime.utcnow().isoformat(' ')
 }
 LICENSES = [
     {
         'url': ' ',
-        'id': int(1),
+        'id': 1,
         'name': ' '
     }
 ]
 CATEGORIES = [
     {
-        'id': int(1),
+        'id': 1,
         'name': 'Lid',
         'supercategory': 'object'
     }
@@ -45,10 +45,10 @@ def create_coco_json(image_dir, annotation_dir, root_dir):
         jpg_image = os.path.join(image_dir, filename)
         image_name = os.path.splitext(filename)[0]
         image_info = {
-            'image_id': image_id,
+            'id': image_id,
             'file_name': filename,
-            'width': int(800),
-            'height': int(600)
+            'width': 800,
+            'height': 600
         }
         coco_output['images'].append(image_info)
         for annotation_name in [annotation for annotation in os.listdir(annotation_dir) if annotation.endswith('jpg') and image_name in annotation]:
@@ -57,23 +57,23 @@ def create_coco_json(image_dir, annotation_dir, root_dir):
             annotation_image = cv2.bitwise_not(annotation_image)
             rle = pycocotools.mask.encode(np.asfortranarray(annotation_image))
             area = pycocotools.mask.area(rle)
-            [x, y, w, h] = cv2.boundingRect(annotation_image)
+            x, y, w, h = cv2.boundingRect(annotation_image)
 
             annotation_info = {
                 'id': annotation_id,
                 'image_id': image_id,
-                'category_id': int(1),
-                'segmentation': rle,
-                'area': area,
-                'bbox': [x, y, w, h],
+                'category_id': 1,
+                'segmentation': str(rle),
+                'area': int(area),
+                'bbox': [int(x), int(y), int(w), int(h)],
                 'bbox_mode': BoxMode.XYWH_ABS,
-                'iscrowd': int(0)
+                'iscrowd': 0
             }
 
             coco_output['annotations'].append(annotation_info)
             annotation_id += 1
         image_id += 1
-    json_string = json.dumps(str(coco_output))
+    json_string = json.dumps(coco_output)
     json_path = os.path.join(root_dir, 'coco.json')
     json_file = open(json_path, 'w')
     json_file.write(json_string)
